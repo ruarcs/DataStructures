840bb1e87d1696ddfad651d3f301fef5114a6682
Ruarc Sorensen <ruarc.sorensen@cylon.com>
2013-05-09 10:23:19 +0100

    Skeleton for a Sorting Array implementation.

---
+++ src/implementations/PriorityQueueSortingArray.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package implementations;
+
+import java.util.Iterator;
+
+import interfaces.PriorityQueue;
+
+/**
+ * @author Ruarc.Sorensen
+ *
+ */
+public class PriorityQueueSortingArray<Item> implements PriorityQueue<Item> 
+{
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+	}
+	
+	public PriorityQueueSortingArray()
+	{
+		
+	}
+	
+	public void push(int priority, Item item)
+	{
+	
+	}
+	
+	public Item pop()
+	{
+		return null;
+		
+	}
+	
+	public int highestPriority()
+	{
+		return 0;
+	
+	}
+	
+	public int lowestPriority()
+	{
+		return 0;
+		
+	}
+	
+	public boolean isEmpty()
+	{
+		return false;
+	
+	}
+	
+	public Item peek()
+	{
+		return null;
+		
+	}
+	
+    public Iterator<Item> iterator()
+    {
+		return null;
+    	
+    }
+
+}
