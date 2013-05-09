a94a25410d7d2f0ef6ed81bacbb6e8feb94a06ab
Ruarc Sorensen <ruarc.sorensen@cylon.com>
2013-05-09 10:23:59 +0100

    A skeleton for the test class for the SortingArray implementation.

---
+++ src/tests/TestSortingArray.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Ruarc.Sorensen
+ *
+ */
+public class TestSortingArray {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	
+
+}
