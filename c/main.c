a9adb09f7c9099d0e2e2ff46c149c7fd10f19776
Ruarc Sorensen <ruarc.sorensen@cylon.com>
2013-05-10 11:30:52 +0100

    Just test compilation with a few calls put into main.

--- c/main.c
+++ c/main.c
@@ -4,4 +4,15 @@ int main()
 {
     int error;
     struct PriorityQueue* pq = init_PriorityQueue(&error);
+
+    int test_data = 999;
+    struct PriorityObject* obj = malloc(sizeof(struct PriorityObject));
+    
+    if(obj)
+    {
+	obj->priority = 100;
+	obj->object_data = &test_data;
+    }
+
+    push(pq, obj, &error);
 }
