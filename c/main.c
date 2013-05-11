161a7a9b44a0ca7cf65fae6bd0be323a98615caf
Ruarc Sorensen <ruarcs@gmail.com>
2013-05-12 00:09:04 +0100

    A slightly better test regime in main. More work to be done!
    Next I'll work on the logic in the priorityQueue file. Still a lot to
    be done there.

--- c/main.c
+++ c/main.c
@@ -1,20 +1,47 @@
 #include "priorityQueue.h"
+#include <assert.h>
+
+struct PriorityObject* createDummyObject(void* test_data, int test_priority)
+{
+    struct PriorityObject* obj = malloc(sizeof(struct PriorityObject));
+    obj->priority = test_priority;
+    obj->object_data = test_data;
+    return obj;
+}
 
 int main()
 {
     int error;
     struct PriorityQueue* pq = init_PriorityQueue(&error);
 
-    int test_data = 999;
-    struct PriorityObject* obj = malloc(sizeof(struct PriorityObject));
+    int test_data1 = 100;
+    int test_data2 = 1000;
+    int test_data3 = 1;
     
-    if(obj)
-    {
-        obj->priority = 100;
-        obj->object_data = &test_data;
-    }
+    struct PriorityObject* obj1 = createDummyObject( (void*)&test_data1, 100);
+    struct PriorityObject* obj2 = createDummyObject( (void*)&test_data2, 1000);
+    struct PriorityObject* obj3 = createDummyObject( (void*)&test_data3, 1);
 
-    push(pq, obj, &error);
+    push(pq, obj1, &error);
+    push(pq, obj2, &error);
+    push(pq, obj3, &error);
+
+
     void* data = pop(pq, &error);
+    assert(data == &test_data2);
+
+    data = pop(pq, &error);
+    assert(data == &test_data1);
+
+    assert(!(isEmpty(pq, &error)));
+    assert(size(pq, &error) == 1);
+
+    data = pop(pq, &error);
+    assert(data == &test_data3);
+
+    assert(isEmpty(pq, &error));
+
     free_PriorityQueue(pq, &error);
+
+    return 0;
 }
